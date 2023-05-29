88628020040e62a64a9416199cbf5d6515531ca6
Golovanova <golovanovaanastasia27@gmail.com>
2023-05-29 11:50:13 -0400

    new

--- file.c
+++ file.c
@@ -3,8 +3,10 @@ int main() {
 	int a = 5;
 	int b = 3;
 	int sum = a + b;
-	int difference = a - b
+	int difference = a - b;
+	int square (a, b);
 	printf("сумма: %d\n", sum);
-	printf("разность: %d/n", difference)
+	printf("разность: %d/n", difference);
+	printf("степень: %d/n", square)
 	return 0;
 }
