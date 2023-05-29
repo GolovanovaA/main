85b9280ad6d393b74c730fe7d8067b1b39329ce5
Golovanova <golovanovaanastasia27@gmail.com>
2023-05-29 10:52:17 -0400

    возвеление в степень

--- file.c
+++ file.c
@@ -3,7 +3,10 @@ int main() {
 	int a = 5;
 	int b = 3;
 	int sum = a + b;
+	int difference = a - b
+	int square = pow(a, b)
 	printf("сумма: %d\n", sum);
 	printf("разность: %d/n", difference)
+	printf("степень: %d/n", square)
 	return 0;
 }
