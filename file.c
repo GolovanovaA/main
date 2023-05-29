e30001cfeed413013a1230c739809a01560601ff
Golovanova <golovanovaanastasia27@gmail.com>
2023-05-29 10:55:32 -0400

    d new

--- file.c
+++ file.c
@@ -4,9 +4,7 @@ int main() {
 	int b = 3;
 	int sum = a + b;
 	int difference = a - b
-	int square = pow(a, b)
 	printf("сумма: %d\n", sum);
 	printf("разность: %d/n", difference)
-	printf("степень: %d/n", square)
 	return 0;
 }
